02c94289fcdf21f31a3225d0e9a059b6fd311ea6
VlakBerr <boro46141@gmail.com>
2024-05-02 19:22:04 +0300

    добавил ответ на третий вопрос

--- 1hw_28_04_24.sql
+++ 1hw_28_04_24.sql
@@ -5,3 +5,7 @@ WHERE Continent = "Africa";
 SELECT Name, SurfaceArea, LifeExpectancy
 FROM country
 WHERE LifeExpectancy > 78.8 AND Continent = "Europe";
+
+SELECT Name, Code, Region
+FROM country
+WHERE SurfaceArea < 10000 AND 1979 <= IndepYear <= 1994;
