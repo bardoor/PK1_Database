18909372f5f0105771aa5027b113f3fa48365b3a
VlakBerr <boro46141@gmail.com>
2024-05-02 19:28:44 +0300

    добавил ответ на четвёртый вопрос

--- 1hw_28_04_24.sql
+++ 1hw_28_04_24.sql
@@ -9,3 +9,7 @@ WHERE LifeExpectancy > 78.8 AND Continent = "Europe";
 SELECT Name, Code, Region
 FROM country
 WHERE SurfaceArea < 10000 AND 1979 <= IndepYear <= 1994;
+
+SELECT Name, LocalName, IndepYear
+FROM country
+WHERE (Name = LocalName AND Population > 5000000) OR (1970 <= IndepYear <= 1980);
