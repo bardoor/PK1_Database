f5b61da3b26a44eaae0f838d88c5719b4d69428c
VlakBerr <boro46141@gmail.com>
2024-05-03 17:06:43 +0300

    добавил в таблицу 10 записей

--- 2hw_05_05_24.sql
+++ 2hw_05_05_24.sql
@@ -5,3 +5,33 @@ CREATE TABLE animals(
     Counteatmaet FLOAT NOT NULL,
     Nickname VARCHAR(100) UNIQUE
 );
+
+INSERT INTO animals
+VALUES(NULL, "01.02.2000", 24, 3.5, "Boby");
+
+INSERT INTO animals
+VALUES(NULL, "05.12.2000", 24, 1.5, "Fred");
+
+INSERT INTO animals
+VALUES(NULL, "11.04.2001", 23, 3.0, "Sophy");
+
+INSERT INTO animals
+VALUES(NULL, "10.11.2000", 24, 5.5, "Bob");
+
+INSERT INTO animals
+VALUES(NULL, "08.08.2000", 24, 1.3, "Sam");
+
+INSERT INTO animals
+VALUES(NULL, "01.02.2002", 22, 2.5, "Lam");
+
+INSERT INTO animals
+VALUES(NULL, "01.09.2000", 24, 4.1, "Cam");
+
+INSERT INTO animals
+VALUES(NULL, "12.12.2004", 20, 1.2, "Pop");
+
+INSERT INTO animals
+VALUES(NULL, "01.02.2023", 1, 0.5, "Kok");
+
+INSERT INTO animals
+VALUES(NULL, "23.11.2014", 10, 11.1, "Uop");
